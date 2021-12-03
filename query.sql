17625d6f97849908204379c57ea0f5705d2ab596
LucasBastos <93799973+LucasBasto@users.noreply.github.com>
2021-12-03 16:15:32 -0300

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,27 @@
+Questão 1- 
+
+SELECT block_number, count (distinct address) FROM `bigquery-public-data.crypto_ethereum.tokens` 
+group by block_number
+
+Questão 2 -
+
+SELECT  cast(block_timestamp as date) as Date_block,
+count (distinct block_number) as Qty_block from `bigquery-public-data.crypto_ethereum.tokens` 
+group by Date_block
+order by date_block desc;
+
+Questão 3- 
+
+SELECT  cast(block_timestamp as date) as Date_block,
+count (distinct block_number) as Valor_Final,
+
+(SELECT Cast(block_timestamp as date) +1 as Date2,
+count(distinct block_number) as block2 FROM `bigquery-public-data.crypto_ethereum.tokens`) as Valor_Inicial,
+
+(Valor_Final/Valor_Inicial -1 )*100 as Variacao 
+
+from `bigquery-public-data.crypto_ethereum.tokens` 
+where cast(block_timestamp as date) > DATE_SUB(current_date(), INTERVAL 15 day)
+group by Date_block
+order by date_block desc;
+
